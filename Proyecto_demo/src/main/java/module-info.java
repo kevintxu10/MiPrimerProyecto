cecd720e3565bee2e3f488c81c7513ce1cf6448a
COMPU-S031\daw1 <cortirowing@gmail.com>
2026-05-08 10:07:21 +0200

    no message

--- Proyecto_demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.proyecto_demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires java.sql;
-
-    opens org.example.proyecto_demo to javafx.fxml;
-    exports org.example.proyecto_demo;
-}
